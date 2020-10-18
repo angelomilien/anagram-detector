2e50e69938a0ef4a808afe050b4625f43362979e
angelo milien <angelomilienjoseph@gmail.com>
2020-10-18 03:41:09 -0400

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require("pry")
+
+class Anagram
+    def initialize(word)
+        @match = word
+    end
+
+    def match(array)
+        array_to_returned = []
+        array.each do |ele|
+        if (ele.chars.sort(&:casecmp).join) == @match.chars.sort(&:casecmp).join
+            array_to_returned << ele
+ 
+        end
+        end
+       array_to_returned
+    end
+
+end
